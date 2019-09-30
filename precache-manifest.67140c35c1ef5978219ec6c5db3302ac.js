2f19410f7d282511abd9be2a7ca5075714dabc13
unknown <AM3114779@AM3114779.c3edu.online>
2019-09-29 20:30:00 -0400

    New Proyect

---
+++ precache-manifest.67140c35c1ef5978219ec6c5db3302ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0895ad6458d3f7358cd30c6080fe242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c99a73011287d32978b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "42a9903e53686a6870b5",
+    "url": "/static/js/2.4917f715.chunk.js"
+  },
+  {
+    "revision": "6c99a73011287d32978b",
+    "url": "/static/js/main.52d00052.chunk.js"
+  },
+  {
+    "revision": "fbbab8c5333faa829672",
+    "url": "/static/js/runtime-main.f88408d8.js"
+  }
+]);
